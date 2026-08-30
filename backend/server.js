50b412ff1c947afa42736fa248d379aa3a7f1d3c
Marco <marcomat447gmail.com>
2026-08-29 22:23:01 -0300

    Fix Render Express 5 route and dependencies

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ app.get('/api/tomtom', async (req, res) => {
 
 // Servir la PWA en Producción
 app.use(express.static(path.join(__dirname, '../dist')));
-app.get('*', (req, res) => {
+app.get('(.*)', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
